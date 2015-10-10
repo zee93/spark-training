875ee73d0652c651da571135240f21e41e5e529e
Ahmed khan <ahmeds.elghareeb@gmail.com>
2015-10-10 14:15:50 +0200

    working on the page rank problem using spark.

--- examples/pairs/pagerank/python/main.py
+++ examples/pairs/pagerank/python/main.py
@@ -1,4 +1,28 @@
 from pyspark import SparkContext, SparkConf
+import os
+
 
 conf = SparkConf().setAppName("test")
 sc = SparkContext(conf=conf)
+_dir = os.path.dirname(os.path.dirname(__file__))
+input_file = sc.textFile(_dir + '/input')
+
+node_contrib = (
+    input_file
+    .map(lambda x: x.split(': '))
+    .map(lambda t: (t[0], t[1].split(', ')))  # (a, (b, c))
+    .flatMap(
+        lambda node: (
+            (node[0], (to, 1/len(node[1]))) for to in node[1]
+            )
+        )
+    )  # (node, (to, contribution))
+node_contrib = node_contrib.map(lambda op: (op[1][0], (op[0], op[1][1])))  # (to, (from, contrib))
+
+node_rank = (
+    input_file
+    .map(lambda x: (x.split(": ")[0], 1))
+)  # (node, rank)
+# ###############################
+for i in range(100):
+    node_rank.leftOutterJoin(node_contrib)
