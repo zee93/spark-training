279d37c571dd4a4159683a1c53bf5001dfaa4305
Ahmed khan <ahmeds.elghareeb@gmail.com>
2015-10-10 14:15:26 +0200

    solving the wordcount problem using spark.

--- examples/pairs/wordcount/python/main.py
+++ examples/pairs/wordcount/python/main.py
@@ -1,4 +1,15 @@
+import os
 from pyspark import SparkContext, SparkConf
 
 conf = SparkConf().setAppName("test")
 sc = SparkContext(conf=conf)
+
+cwd = os.path.dirname(os.path.dirname(__file__))
+words_count_rdd = (sc.
+                   textFile(cwd + '/input')
+                   .flatMap(lambda l: l.split(' '))
+                   .filter(lambda x: x != '' and x != '.')
+                   .map(lambda x: (x.lower(), 1))
+                   .reduceByKey(lambda a, b: a + b)
+                   )
+words_count_rdd.saveAsTextFile('output')
--- examples/pairs/wordcount/scala/Main.scala
+++ examples/pairs/wordcount/scala/Main.scala
@@ -4,7 +4,7 @@ object Main {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
+    val words_count_rdd = sc.textFile("..\\input").flatMap(_.split).map(_ -> 1).reduceByKey(a,b => a + b)
 
-  
   }
 }
